8b701eeec75f79bf6773b7ecea2c2aab58033563
Basil <ohuruogubasil@gmail.com>
2022-12-05 21:59:09 +0100

    added:e-shop-server

--- src/components/features/productApi.js
+++ src/components/features/productApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const productsApi = createApi({
   reducerPath: "productsApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.BASE_URL}),
   endpoints: (builder) => ({
     getAllProducts: builder.query({
       query: () => `products`,
